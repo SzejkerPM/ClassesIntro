a65d9c6006ae5e2fd9e88730099985d389ad9700
SzejkerPM <ghaawk@wp.pl>
2026-04-19 13:16:51 +0200

    Added Hero List in for loop and updated git ignore (*.user)

--- ClassesIntro/Program.cs
+++ ClassesIntro/Program.cs
@@ -1,4 +1,5 @@
-﻿using ClassesIntro;
+﻿using System.Diagnostics;
+using ClassesIntro;
 // See https://aka.ms/new-console-template for more information
 // string playerAvatar = "@";
 // Console.WriteLine(playerAvatar);
@@ -6,32 +7,50 @@
 Vector2 startingPosition = new Vector2(4, 2);
 Player hero = new Player(startingPosition);
 
+var heroClones = new List<Player>();
+heroClones.Add(hero);
+heroClones.Add(new Player(startingPosition));
+
+
+foreach (var clone in heroClones)
+{
+    clone.Display();
+}
+
 while (true)
 {
-    hero.Display();
-    ConsoleKeyInfo keyInfo = Console.ReadKey(true); //true = ignore (przechwyć input, ale nie wyświetlaj go w konsoli)
+    for (int i = 0; i < heroClones.Count; i++)
+    {
+        Player currentHeroClone = heroClones[i];
+        
+        currentHeroClone.Display();
+        
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true); //true = ignore (przechwyć input, ale nie wyświetlaj go w konsoli)
+        
+        currentHeroClone.ClearPosition();
+            
+            if (keyInfo.Key == ConsoleKey.A)
+            {
+                // ruch w lewo
+                currentHeroClone.Move(-1, 0);
+            }
+            else if (keyInfo.Key == ConsoleKey.D)
+            {
+                // ruch w prawo
+                currentHeroClone.Move(1, 0);
+            }
+            else if (keyInfo.Key == ConsoleKey.W)
+            {
+                // ruch w górę
+                currentHeroClone.Move(0, -1);
+            }
+            else if (keyInfo.Key == ConsoleKey.S)
+            {
+                // ruch w dół
+                currentHeroClone.Move(0, 1);
+            }
+            
+            currentHeroClone.Display();
+    }
 
-    hero.ClearPosition();
-    
-    if (keyInfo.Key == ConsoleKey.A)
-    {
-        // ruch w lewo
-        hero.Move(-1, 0);
-    }
-    else if (keyInfo.Key == ConsoleKey.D)
-    {
-        // ruch w prawo
-        hero.Move(1, 0);
-    }
-    else if (keyInfo.Key == ConsoleKey.W)
-    {
-        // ruch w górę
-        hero.Move(0, -1);
-    }
-    else if (keyInfo.Key == ConsoleKey.S)
-    {
-        // ruch w dół
-        hero.Move(0, 1);
-    }
-    
 }
